0cbc95bfe2b9a7f7a579bf0a23de386a4aa50d6a
Saiful Ajom Khan <sajomkhan@gmail.com>
2023-12-20 00:29:01 +0600

    updated

--- app/_components/Navbar.jsx
+++ app/_components/Navbar.jsx
@@ -61,7 +61,7 @@ export default function Navbar() {
                   {name}
                 </p>
               ) : (
-                <div>
+                <div className="sm:flex sm:gap-4">
                   <a
                     className="block rounded-md bg-teal-600 px-5 py-2.5 text-sm font-medium text-white transition hover:bg-teal-700"
                     href="/login"
